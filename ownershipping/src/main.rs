dcaf91b088dc77462f3bc2f7566aa22c4284ea3f
Devraj Singh <aol.devraj@aol.com>
2025-05-16 18:11:52 +0530

    ownershipping almost finished

--- ownershipping/src/main.rs
+++ ownershipping/src/main.rs
@@ -48,18 +48,90 @@ fn main() {
 }
 
 use std::mem::take;
-*/
-fn main() {
-    let s1 = give_back();
-    let s2 = String::from("hello");
-    let s3 = take_back(s2);
-    println!("{}, {}", s1, s3);
-}
 
+// fn main() {
+//     let s1 = give_back();
+//     let s2 = String::from("hello");
+//     let s3 = take_back(s2);
+//     println!("{}, {}", s1, s3);
+// }
+//
 fn give_back() -> String {
     let some_string = String::from("yours");
     some_string
 }
 fn take_back(a_string: String) -> String {
     a_string
+}
+fn main() {
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{s2}' is {len}.");
+}
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+    (s, length)
+}
+this is called tuple code
+
+ fn main() {
+     let s1 = String::from("hello");
+     let s1_len = calculate_len(&s1);
+     println!("the length of {} is {}", s1, s1_len);
+ }
+fn calculate_len(s: &String) -> usize {
+    s.len()
+}
+
+fn main() {
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("string value is {}", s);
+
+}
+fn change(some_string: &mut String) {
+    some_string.clear();
+}
+
+fn main() {
+    let mut s = String::from("hello");
+    let r1 = &mut s;
+    r1.push_str(", world");
+    println!("{}", r1);
+    let r2 = &mut s;
+    r2.push_str(", batman");
+    println!("{}", r2);
+}
+
+fn main() {
+    let mut s = String::from("hello");
+
+    let mut original = s.clone();  // Clone the original value
+
+    let r1 = &mut s;
+    r1.push_str(", world");
+    println!("{}", r1);        // Prints: hello, world
+
+    let r2 = &mut original;     // Use the unmodified clone
+    r2.push_str(", batman");
+    println!("{}", r2);        // Prints: hello, batman
+}
+
+fn main() {
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+        r1.push_str(" there");
+        println!("r1={}", r1);
+        
+    }
+    {
+        let r2 = &mut s;
+        r2.push_str(" human");
+        println!("r2={}", r2);
+    }
+}
+*/
+fn main() {
+    
 }
